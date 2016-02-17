6a741bc964ea92de96f12002f56ad3a32a983f50
wuthmone <whlaing10@gmail.com>
2016-02-17 16:16:51 +0800

    Modifiled files

--- insertdata.sql
+++ insertdata.sql
@@ -1,3 +1,5 @@
+
+
 Insert into EMPLOYEE (EMPNO,EMPNAME,DEPARTMENT,EMAIL,PHONE) values ('E100','Chuck Coordinator','Administration','chuck@colorado.edu','3-1111');
 Insert into EMPLOYEE (EMPNO,EMPNAME,DEPARTMENT,EMAIL,PHONE) values ('E101','Mary Manager','Football','mary@colorado.edu','5-1111');
 Insert into EMPLOYEE (EMPNO,EMPNAME,DEPARTMENT,EMAIL,PHONE) values ('E102','Sally Supervisor','Planning','sally@colorado.edu','3-2222');
